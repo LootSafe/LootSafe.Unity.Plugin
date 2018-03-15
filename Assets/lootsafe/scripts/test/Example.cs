1e8df8f64b152f34d87dede0ca40373c60526b6a
SoftDevAndy <sweeneyandrew.softdev@gmail.com>
2018-03-15 14:00:35 -0400

    Example * Added a minimal example script

---
+++ Assets/lootsafe/scripts/test/Example.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour {
+
+	void Start(){
+    
+        string apiUrl = "http://localhost:1337/v1";
+        string apiKey = "pWpzWuxoKUKAmlHc0wPi7lFS38FTth";
+
+        LootSafe lootsafe = new LootSafe(apiUrl, apiKey);
+
+        StartCoroutine(lootsafe.crafter.getCraftables((status) => {
+
+            Debug.Log("lootsafe.crafter.getCraftables\n" + status.ToString());
+
+            // Do stuff with status string.
+        }));
+    }
+}
